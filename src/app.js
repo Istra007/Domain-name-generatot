0d4eea75eb7f182218c23689ab537df6987e637f
Istra007 <irinastrakhova007@gmail.com>
2021-09-02 16:12:45 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let four = [".com", ".net", ".us"];
+
+  let string = "";
+
+  for (let x of pronoun) {
+    for (let y of adj) {
+      for (let a of noun) {
+        for (let b of four) {
+          document.querySelector("#irina").innerHTML +=
+            "<li>" + x + y + a + b + "</li>";
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
